f4ada3a4c3d939356447a66226cbf7c8bb4ae15b
Mark-Justine-Go <118537589+Mark-Justine-Go@users.noreply.github.com>
2026-03-17 15:40:34 +0800

    Finish the project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# rock-paper-scissor
+# Rock Paper Scissor
+
+This project consists of 1 page showcasing a simple game of rock paper scissors using javascript.
+
+Skills demonstrated:
+
+- using functions to divide a problem into a set of a more approachable sub problems
+- utilizing the console and different error types to easily debug problems
---
+++ script.js
@@ -0,0 +1,71 @@
+humanScore = 0;
+computerScore = 0;
+
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random()*3);
+
+    switch(randomNum){
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissor";
+    }
+}
+
+function getHumanChoice(){
+    return prompt("Enter your stance: ").toLowerCase();
+}
+
+function playRound(human, computer){
+    switch(human){
+        case "rock":
+            if(computer === "paper"){
+                console.log("You lose, paper beats rock!");
+                computerScore++;
+            }else if(computer === "scissor"){
+                console.log("You win, rock beats scissor!");
+                humanScore++;
+            }else{
+                console.log("Tie! both played rock");
+            }
+            break
+        case "paper":
+            if(computer === "rock"){
+                console.log("You win, paper beats rock!");
+                humanScore++;
+            }else if(computer === "scissor"){
+                console.log("You lose, scissor beats paper!");
+                computerScore++;
+            }else{
+                console.log("Tie! both played paper");
+            }
+            break
+        case "scissor":
+            if(computer === "rock"){
+                console.log("You lose, rock beats scissor!");
+                computerScore++;
+            }else if(computer === "paper"){
+                console.log("You win, scissor beats paper!");
+                humanScore++;
+            }else{
+                console.log("Tie! both played scissor");
+            }
+            break
+    }
+
+    console.log("Current Scores");
+    console.log(`Human: ${humanScore}\nComputer: ${computerScore}`);
+}
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        let player = getHumanChoice();
+        let computer = getComputerChoice();
+
+        playRound(player,computer);
+    }
+}
+
+playGame();
